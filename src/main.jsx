313f6c32f2e0f9c6f6d03cd41594b42731b1cdb3
FrancescoLimosani <71842759+FrancescoLimosani@users.noreply.github.com>
2026-06-24 00:01:51 +0200

    take 8

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  delete sessionStorage.redirect;
+  history.replaceState(null, null, redirect);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/Riccard01.github.io">
