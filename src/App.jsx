56501251121b720c813c4954d4665ccfb53f0cd1
Riccardo01 <riccardo@leggerotours.com>
2026-06-29 12:56:04 +0200

    Prova block desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
- import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Book from './pages/Book';
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/book" element={<Book />} />
-    </Routes>
+    <>
+      {/* Schermata di blocco totalmente indipendente */}
+      <div id="desktop-blocker">
+        <div className="blocker-content">
+          <h1 className="blocker-title">Mobile view required</h1>
+          <p className="blocker-desc">
+            For a better and smoother navigation experience, this platform is optimized exclusively for handheld devices.
+          </p>
+          <span className="blocker-action">Please press F12 to continue.</span>
+        </div>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/book" element={<Book />} />
+      </Routes>
+    </>
   );
 }
 
